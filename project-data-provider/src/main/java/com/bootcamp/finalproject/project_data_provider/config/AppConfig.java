807dfefe15ac99617e71c179a23a3def68c7838a
Victor Yiu <yiulokhei91@gmail.com>
2025-10-27 11:48:29 +0800

    202510271148

--- project-data-provider/src/main/java/com/bootcamp/finalproject/project_data_provider/config/AppConfig.java
+++ project-data-provider/src/main/java/com/bootcamp/finalproject/project_data_provider/config/AppConfig.java
@@ -8,6 +8,15 @@ import org.springframework.web.client.RestTemplate;
 public class AppConfig {
   @Bean
   RestTemplate restTemplate() {
-    return new RestTemplate();
+    RestTemplate restTemplate = new RestTemplate();
+    restTemplate.getInterceptors().add((request, body, execution) -> {
+      request.getHeaders().set("User-Agent",
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36");
+      request.getHeaders().set("Accept", "application/json");
+      request.getHeaders().set("Accept-Language", "en-US,en;q=0.9");
+      return execution.execute(request, body);
+    });
+
+    return restTemplate;
   }
 }
