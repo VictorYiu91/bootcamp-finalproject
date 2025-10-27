70c7cbfa3a28cd6ee910c0f9cc7122ed17824875
Victor Yiu <yiulokhei91@gmail.com>
2025-10-27 15:20:31 +0800

    202510271520

--- project-data-provider/src/main/java/com/bootcamp/finalproject/project_data_provider/controller/ProjectDataOperation.java
+++ project-data-provider/src/main/java/com/bootcamp/finalproject/project_data_provider/controller/ProjectDataOperation.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.bootcamp.finalproject.project_data_provider.dto.QuoteDto;
 import com.bootcamp.finalproject.project_data_provider.model.dto.CompanyDTO;
 
-public interface ProjectDataController {
+public interface ProjectDataOperation {
   @GetMapping(value = "/quote")
   QuoteDto getQuote(@RequestParam String symbol);
 
--- project-data-provider/src/main/java/com/bootcamp/finalproject/project_data_provider/controller/impl/ProjectDataController.java
+++ project-data-provider/src/main/java/com/bootcamp/finalproject/project_data_provider/controller/impl/ProjectDataController.java
@@ -2,14 +2,14 @@ package com.bootcamp.finalproject.project_data_provider.controller.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
-import com.bootcamp.finalproject.project_data_provider.controller.ProjectDataController;
+import com.bootcamp.finalproject.project_data_provider.controller.ProjectDataOperation;
 import com.bootcamp.finalproject.project_data_provider.dto.QuoteDto;
 import com.bootcamp.finalproject.project_data_provider.model.dto.CompanyDTO;
 import com.bootcamp.finalproject.project_data_provider.model.dto.QuoteDTO;
 import com.bootcamp.finalproject.project_data_provider.service.ProjectDataService;
 
 @RestController
-public class ProjectDataOperation implements ProjectDataController {
+public class ProjectDataController implements ProjectDataOperation {
   @Autowired
   private ProjectDataService projectDataService;
 
