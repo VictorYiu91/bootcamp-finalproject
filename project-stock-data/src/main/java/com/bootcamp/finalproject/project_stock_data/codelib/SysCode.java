b3d9bb76faed40662674933464c1665a88a5b0fe
VictorYiu91 <yiulokhei91@gmail.com>
2025-10-30 19:23:02 +0800

    Add files via upload

---
+++ project-stock-data/src/main/java/com/bootcamp/finalproject/project_stock_data/codelib/SysCode.java
@@ -0,0 +1,23 @@
+package com.bootcamp.finalproject.project_stock_data.codelib;
+
+public enum SysCode {
+  OK(0, "OK."), //
+  FAIL(99999, "Fail."), //
+  ;
+
+  private Integer code;
+  private String message;
+
+  private SysCode(Integer code, String message) {
+    this.code = code;
+    this.message = message;
+  }
+
+  public Integer getCode() {
+    return this.code;
+  }
+
+  public String getMessage() {
+    return this.message;
+  }
+}
